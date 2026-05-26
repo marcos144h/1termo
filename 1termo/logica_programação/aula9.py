4d6d98aafda3a6bf4d32343aa7316f172b346b5f
marcos vailant <santosmarcosviniciosantos@gmail.com>
2026-05-26 09:17:35 -0300

    26/05/26

--- 1termo/logica_programação/aula9.py
+++ 1termo/logica_programação/aula9.py
@@ -1,15 +1,7 @@
-print("sistema robotico")
+#print("sistema robotico")
 
-qualidade_da_peça=input("qual a qualidade da peça")
+#qualidade_da_peça=input("qual a qualidade da peça")
 
-if qualidade_da_peça =="verde":
-    print("de 50 passos ate passos")
-    print("agache a pegue a peça")
-    print("vire na direção que voce veio e leve a peça ate o programador")
-elif qualidade_da_peça == "vermelha":
-    print("50 passos ate a peça")
-    print("agache e pegue a peça")
-    print("vire na direção que voce veio e leveate o professor")
-else:
-    print("não faça nada")
+#######  print("não faça nada")
           
+
